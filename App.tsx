0aa881cd3a7e6365b235cbc57087cb75be1befe0
Derek Martin <derek.martin@metrictheory.com>
2020-06-18 16:54:47 -0700

    :heart_eyes: init the repo

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { 
+  View,
+  StyleSheet,
+  Text
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>My Trading app</Text>
+    </View>
+  )
+};
+
+export default App;
